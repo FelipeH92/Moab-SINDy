2f1a6d56f013598f3ff90bae82e92cd713e775be
FelipeH92 <felipe.nascimento@ee.ufcg.edu.br>
2021-03-05 18:06:38 -0300

    SINDy aplicado aos dados das simulações

--- README.md
+++ README.md
@@ -7,9 +7,8 @@ Como estudo de caso do algoritmo SINDy, diferentes abordagens são realizadas:
 1. Modelo simplificado do MOAB e sua aplicação com o SINDy.
 2. Modelo padrão do MOAB e sua aplicação com o SINDy.
 3. Uso apenas de dados da posição da bola (x e y) da simulação em Simulink (Deve dar errado).
-4. Uso de dados da posição da bola (x e y) e da entrada da plataforma (\theta_x e \theta_y) n SINDy Control.
-5. Uso de dados da posição da bola (x e y) e da entrada da plataforma (\theta_x e \theta_y) n SINDy Control, e uso das velocidades da simulação.
-6. Uso de dados de um sistema _ball on plate_ real.
+4. Uso de dados da posição da bola (x e y) e da entrada da plataforma (\theta_x e \theta_y) n SINDy Control, e uso das velocidades da simulação.
+5. Uso de dados de um sistema _ball on plate_ real.
 
 # Dependencias necessários
 
---
+++ src/MOAB_Simulink_Data.py
@@ -0,0 +1,67 @@
+import os
+import numpy as np
+import utils.data_manager as dm
+import pysindy as ps
+
+# O estado (posição e velocidade em x e y) e a entrada (angulo x e y do plate)
+feature_names = ['x', 'dx', 'y', 'dy', 'theta_x', 'theta_y']
+output_names = ['dx', 'ddx', 'dy', 'ddy']
+
+def fit_moab_sindy(x_0, t_train, x_0_test, t_test, u_train = None, u_test = None, otimizador = ps.STLSQ(threshold=0.5, alpha=.05)):
+
+    # Definindo a biblioteca de funções como funções polinomiais de grau 5
+    library = ps.PolynomialLibrary(degree=5, include_interaction=False)
+
+    # Otimizador
+    optimizer = otimizador
+
+    # Utilizando o SINDy
+    model = ps.SINDy(feature_names=feature_names, feature_library=library, optimizer=optimizer)
+    model.fit(x_0, t=t_train, u=u_train)
+    model.print(lhs=output_names)
+
+    print('Model score): %f' %model.score(x_0_test, u=u_test, t=t_test))
+    print('\n')
+
+    return model
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+data_path = dir_path + "/data/Output_Sample_Time_1e4"
+
+for i in range(1,10):
+
+    x_train, t_train, u_train, dx_train = dm.read_batch_moab(data_path, i, reduced = True)
+
+    x_train_final = np.zeros((x_train.shape[0],4))
+    x_train_final[:,0] = x_train[:,0]
+    x_train_final[:,2] = x_train[:,1]
+    x_train_final[:,1] = dx_train[:,0]
+    x_train_final[:,3] = dx_train[:,1]
+
+    x_test, t_test, u_test, dx_test = dm.read_batch_moab(data_path, i+1, reduced = True)
+
+    x_test_final = np.zeros((x_test.shape[0],4))
+    x_test_final[:,0] = x_test[:,0]
+    x_test_final[:,2] = x_test[:,1]
+    x_test_final[:,1] = dx_test[:,0]
+    x_test_final[:,3] = dx_test[:,1]
+
+    fit_moab_sindy(x_train_final,t_train,x_test_final,t_test,u_train=u_train, u_test=u_test)
+
+x_train, t_train, u_train, dx_train = dm.read_batch_moab(data_path, 8, reduced = True)
+
+x_train_final = np.zeros((x_train.shape[0],4))
+x_train_final[:,0] = x_train[:,0]
+x_train_final[:,2] = x_train[:,1]
+x_train_final[:,1] = dx_train[:,0]
+x_train_final[:,3] = dx_train[:,1]
+
+x_test, t_test, u_test, dx_test = dm.read_batch_moab(data_path, 6, reduced = True)
+
+x_test_final = np.zeros((x_test.shape[0],4))
+x_test_final[:,0] = x_test[:,0]
+x_test_final[:,2] = x_test[:,1]
+x_test_final[:,1] = dx_test[:,0]
+x_test_final[:,3] = dx_test[:,1]
+
+fit_moab_sindy(x_train_final,t_train,x_test_final,t_test,u_train=u_train, u_test=u_test)
